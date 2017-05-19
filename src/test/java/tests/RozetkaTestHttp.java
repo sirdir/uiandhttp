be445edc08151fc3d5e492c700a241a3f92999e8
sirdir <phddir@gmail.com>
2017-05-19 08:06:34 +0300

    redone to regexp

--- src/test/java/tests/RozetkaTestHttp.java
+++ src/test/java/tests/RozetkaTestHttp.java
@@ -1,14 +1,11 @@
 package tests;
 
-import com.jayway.jsonpath.JsonPath;
-import io.restassured.http.Cookie;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import utils.Parser;
 
 import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
 
 /**
  * Created by sirdir on 18.05.17.
@@ -21,21 +18,12 @@ public class RozetkaTestHttp extends BaseTest {
         Response searchResult = get(url);
         url = Parser.rozetkaHiddenItem(searchResult.getBody().asString(), "$.productHref");
 
-
         String itemPage = get(url).getBody().asString();
         String usdPrice = Parser.rozetkaHiddenItem(itemPage, "$.productPrice");
         System.out.println("price for article with id " + id + " is " + usdPrice + "$");
 
-        String productId = Parser.rozetkaHiddenItem(itemPage, "$.productID");
-
-        url = BASE_URL + "/recent_goods/action=getRecentGoods;goods_ids=" + productId + "/";
-        System.out.println(url);
-        Response response = get(url);
-        String json = response.getBody().asString();
-
-        String countCommentsStr = JsonPath.read(json, "$.content[0].count_comments");
-        Integer countComments = Integer.valueOf(countCommentsStr);
-        Integer userRating = JsonPath.read(json, "$.content[0].users_rating");
+        Integer countComments = Parser.rozetkaDirtyRegexp("reviewCount\\\"\\scontent=\\\"\\d{1,}\\\"", itemPage);
+        Integer userRating = Parser.rozetkaDirtyRegexp("ratingValue\\\"\\scontent=\\\"\\d{1,}\\\"", itemPage);
 
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(countComments, expCommentsQuantity, "review count ");
